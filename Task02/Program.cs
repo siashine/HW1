5cadad676772d3bbf8d85d7bb63116e61e9f27e8
Anastasia <kazantseva_nastya_95@mail.ru>
2022-10-07 15:16:10 +0500

    Task04-done

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -3,7 +3,7 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-
+Console.Clear();
 Console.WriteLine("Введите первое число: ");
 int numberA = int.Parse(Console.ReadLine());
 
@@ -12,8 +12,8 @@ int numberB = int.Parse(Console.ReadLine());
 
 if (numberA>numberB)
 {
-  Console.Write("Больше это число: " + numberA);
+  Console.WriteLine("Больше это число: " + numberA);
 }
 else{
-  Console.Write("Больше это число: " + numberB);
+  Console.WriteLine("Больше это число: " + numberB);
 }
--- Task08/Program.cs
+++ Task08/Program.cs
@@ -3,7 +3,7 @@
 // числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
-
+Console.Clear();
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 
